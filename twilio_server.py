18839ac27fcf0651b9dc8591dec7251275d4a324
Michael Lin <michaelv03@gmail.com>
2018-04-25 23:59:23 -0700

    got broadcasting to work

--- twilio_server.py
+++ twilio_server.py
@@ -2,6 +2,8 @@
 import redis
 from flask import Flask, request, url_for, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
+import time
+import re
 
 flask_app = Flask(__name__)
 flask_app.config['SECRET_KEY'] = 'top-secret!'
@@ -9,23 +11,36 @@ flask_app.config['SECRET_KEY'] = 'top-secret!'
 # Redis client
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 p = r.pubsub()
+p.subscribe('broadcast_rsp')
+
+# Constants
+TIMEOUT = 20
+patt = re.compile('Water (.*)')
 
 @flask_app.route("/", methods=['GET','POST'])
 def sms_service_request():
 	"""Respond to incoming messages with a friendly SMS."""
 		
-	recvd_msg = request.form['Body']
+	rcv_msg = request.form['Body']
 
 	# Start our response
 	resp = MessagingResponse()
 
-	in_dic = actions.get(recvd_msg)
+	match = patt.search(rcv_msg)
+
+	if (match):
+		rcv_msg = "Water"
+
+	in_dic = actions.get(rcv_msg)
 	
 	# if command is present in dictionary
 	if (in_dic == None):
  	   action_msg = "I only understand the following commands: " + str(actions.keys())
 	else:
- 	   action_msg = actions[recvd_msg]()
+		if (match):
+			action_msg = actions[rcv_msg](match.group(1))
+		else:
+			action_msg = actions[rcv_msg]()
 
 	# Add a message
 	resp.message("Ahoy! Thanks so much for your message. " + action_msg)
@@ -36,14 +51,20 @@ def sms_ahoy_reply():
 	return "waterPIC will keep your plants moisty and tasty"
 
 def plant_status():
-	return "plants are alright"
+	r.publish('to_broadcast', 'status')
+	t_start = time.time()
+	msg = p.get_message()
+	while(not msg and (time.time()-t_start) < TIMEOUT):
+		msg = p.get_message()
+	return "plants status\n" + msg['data']
 
-def water_chili():
-	return "watering chili plant!"
+def water(plant):
+	r.publish('to_broadcast', 'water ' + plant)
+	return "watering " + plant + " plant!"
 
 actions = { "Hi"	 : sms_ahoy_reply,   \
 			"Status" : plant_status,	 \
-			"Water chili" : water_chili }
+			"Water" : water}
 
 
 if __name__ == "__main__":
